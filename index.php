db02b4073f203148cb1ec59b71c7c91f7546ceef
Qatrunada Qori Darwati <57135989+qqdnada@users.noreply.github.com>
2021-08-04 09:15:40 +0800

    change local bootstrap

--- index.php
+++ index.php
@@ -8,8 +8,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - Programming YHC</title>
 
-    <!-- Style -->
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
 </head>
 <body>
     <div class="container">
